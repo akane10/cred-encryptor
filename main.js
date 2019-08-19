a834aff688060f85ce8af5212c9f5fe1704c2004
yapie <targaryen.akane@gmail.com>
2019-08-19 17:55:39 +0700

    tidy up

--- main.js
+++ main.js
@@ -5,15 +5,11 @@ const show = require('./src/show');
 const deleteData = require('./src/delete');
 
 (function() {
-  const ARGS = process.argv;
-  const arg = ARGS[2];
+  const [_, __, command] = process.argv;
 
-  if (arg === 'add') return add();
-
-  if (arg === 'show') return show();
-
-  if (arg === 'delete') return deleteData();
+  if (command === 'add') return add();
+  if (command === 'show') return show();
+  if (command === 'delete') return deleteData();
 
   console.log('oopss unknown command!!');
-  process.exit(1);
 })();
