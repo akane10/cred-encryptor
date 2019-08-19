c20b7d414e2d46abd5dbfd89e9c2750d8e27c11b
yapie <targaryen.akane@gmail.com>
2019-08-19 18:28:55 +0700

    catch err

--- src/helpers.js
+++ src/helpers.js
@@ -3,9 +3,13 @@ const fs = require('fs');
 const toJson = data => JSON.stringify(data, null, 2);
 
 function read(pathData) {
-  const data1 = fs.readFileSync(pathData, 'utf-8');
-  const data = JSON.parse(data1);
-  return data;
+  try {
+    const data1 = fs.readFileSync(pathData, 'utf-8');
+    const data = JSON.parse(data1);
+    return data;
+  } catch (e) {
+    console.log('whoaaa', e.message);
+  }
 }
 
 const addJson = newData => data => {
@@ -16,9 +20,13 @@ const addJson = newData => data => {
 };
 
 function editFile(path, fn) {
-  const data = fs.readFileSync(path, 'utf-8');
-  const newData = fn(data);
-  fs.writeFileSync(path, newData, 'utf-8');
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+    const newData = fn(data);
+    fs.writeFileSync(path, newData, 'utf-8');
+  } catch (e) {
+    console.log('whoaaa', e.message);
+  }
 }
 
 module.exports = {
